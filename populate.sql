9b6404450819fec666a9da1991c34f8460012a41
Jguer <me@jguer.space>
2018-10-26 11:12:15 +0100

    Add dml

---
+++ populate.sql
@@ -0,0 +1,63 @@
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (293121444,
+        'John Smith',
+        'Rua Dona Amelia,9, Picheleira',
+        'Lisboa',
+        '1820-021');
+
+
+insert into veterinary (VAT,specialization,bio)
+values (123121444,
+        "surgery",
+        "A very nice man");
+
+
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (493232134,
+        'Jorge Serra',
+        'Rua da Damaia,2, Damaia',
+        'Amadora',
+        '1904-192');
+
+
+insert into client (VAT)
+values (493232134);
+
+
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (921383212,
+        'Nuno Neves',
+        'Rua Alvaro Cunhal,182, Beja',
+        'Beja',
+        '1204-222');
+
+
+insert into client (VAT)
+values (921383212);
+
+
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (482121911,
+        'Damasio Silva',
+        'Rua Santo Estevao,13, Soidos de Cima',
+        'Loulé',
+        '2204-019');
+
+
+insert into client (VAT)
+values (482121911);
+
+
+insert into species (name,description)
+values ("dobberman",
+        "very german");
+
+
+insert into species (name,description)
+values ("tibetan mastiff",
+        "very tibetan");
+
+
+insert into species (name,description)
+values ("mastiff",
+        "not tibetan");
