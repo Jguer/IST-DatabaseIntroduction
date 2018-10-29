1dba790a06fa19596f8f6cbabe029eea9614ef44
Jguer <me@jguer.space>
2018-10-29 17:07:47 +0000

    Add animals

--- populate.sql
+++ populate.sql
@@ -7,7 +7,7 @@ values (293121444,
 
 
 insert into veterinary (VAT,specialization,bio)
-values (123121444,
+values (293121444,
         "surgery",
         "A very nice man");
 
@@ -48,9 +48,82 @@ insert into client (VAT)
 values (482121911);
 
 
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (812924465,
+        'Lajos Kossuth',
+        'Rua Sarmento Rosa 92',
+        'Loulé',
+        '2204-123');
+
+
+insert into veterinary (VAT,specialization,bio)
+values (812924465,
+        "surgery",
+        "A very regal man");
+
+
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (146252131,
+        'Alfred I, Prince of Windisch-Gratz',
+        'Rua Vieira Luis Filipe 19',
+        'Porto',
+        '6451-035');
+
+
+insert into veterinary (VAT,specialization,bio)
+values (146252131,
+        "analysis",
+        "A very regal man");
+
+
+insert into client (VAT)
+values (146252131);
+
+
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (782333333,
+        'Jair Bolsonaro',
+        'Avenida da Liberdade 1',
+        'Brasilia',
+        '9324-031');
+
+
+insert into client (VAT)
+values (782333333);
+
+
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (692323322,
+        'Dilma Rouseff',
+        'Praça dos Combatentes 31',
+        'Rio de Janeiro',
+        '8208-818');
+
+
+insert into client (VAT)
+values (692323322);
+
+
+insert into person (VAT,name,address_street,address_city,address_zip)
+values (121212746,
+        'Ricardo Pereira',
+        'Rua Jorge da Cunha 143',
+        'Abrunheira',
+        '1821-131');
+
+
+insert into client (VAT)
+values (121212746);
+
+
 insert into species (name,description)
 values ("dobberman",
         "very german");
+
+
+insert into species (name,description)
+values ("chiwawa",
+        "very... small");
 
 
 insert into species (name,description)
@@ -61,3 +134,78 @@ values ("tibetan mastiff",
 insert into species (name,description)
 values ("mastiff",
         "not tibetan");
+
+
+insert into species (name,description)
+values ("Cao de agua",
+        "Fuzzy and warm");
+
+
+insert into animal
+values ("bobi",
+        482121911,
+        "mastiff",
+        "black",
+        "male",
+        1995,
+        23);
+
+
+insert into animal
+values ("gros bobi",
+        482121911,
+        "dobberman",
+        "white",
+        "female",
+        1999,
+        19);
+
+
+insert into animal
+values ("joli",
+        921383212,
+        "chiwawa",
+        "black",
+        "male",
+        2013,
+        5);
+
+
+insert into animal
+values ("socio",
+        493232134,
+        "mastiff",
+        "chestnut",
+        "male",
+        2012,
+        6);
+
+
+insert into animal
+values ("kafka",
+        493232134,
+        "tibetan mastiff",
+        "black",
+        "male",
+        2010,
+        8);
+
+
+insert into animal
+values ("ferdinand",
+        146252131,
+        "Cao de agua",
+        "grey",
+        "male",
+        2017,
+        1);
+
+
+insert into animal
+values ("poe",
+        692323322,
+        "Cao de agua",
+        "grey",
+        "female",
+        2002,
+        16);
