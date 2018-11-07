c8102503c88282c538ff21244a9ad836cd7e2884
carlosaleluia <32376486+carlosaleluia@users.noreply.github.com>
2018-11-07 17:52:28 +0000

    forgot smt

--- query.sql
+++ query.sql
@@ -111,15 +111,10 @@ from diagnosis_code,
     group by consult_diagnosis.code) as D
 where diagnosis_code.code = D.c 
       and D.n >= all(select count(consult_diagnosis.code) as n
-                  from generalization_species as dog_species,
-                      species as sub,
-                      animal inner join consult_diagnosis 
+                  from animal inner join consult_diagnosis 
                       on animal.name = consult_diagnosis.name 
                          and animal.vat = consult_diagnosis.vat_owner
-                  where animal.species_name = sub.name
-                        and dog_species.name2 = "dog"
-                        and dog_species.name1 = sub.name
-                        and sub.name = D.s
+                  where animal.species_name = D.s
                   group by consult_diagnosis.code
                   );
 
